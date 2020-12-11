a242e3d77f87ad3672b6f196f3ab2aa910916239
tram421 <maitram0291@gmail.com>
2020-12-12 03:08:26 +0700

    Bai tap Relatetive Layout

---
+++ app/src/main/java/com/tram/relatetivelayout24112020/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tram.relatetivelayout24112020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
